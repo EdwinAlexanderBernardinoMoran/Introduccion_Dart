eda601cf0bd0d22d799ea6c02df00ad7ce088f72
Edwin Alexander Bernardino Moran <edwinalexanderbernardinomoran@gmail.com>
2023-06-26 17:23:59 -0600

    Tipos de datos - Numeros y String

---
+++ 01_tipos_de_datos.dart
@@ -0,0 +1,32 @@
+// void main() {
+//   print("Hello Word !!");
+// }
+
+void main() {
+// String
+// ----------------------------------------------------
+
+  // var nombre = "Edwin";
+  // var apellido = "Bernardino";
+
+  // String nombre = "Edwin";
+  // String apellido = "Bernardino";
+
+  // final nombre = "Edwin";
+  // final apellido = "Bernardino";
+
+  const nombre = "Edwin";
+  const apellido = "Bernardino";
+
+// Template String
+  print('$nombre $apellido');
+
+  // Numeros
+  // ---------------------------------------------------
+
+  int empleados = 10;
+  double salario = 10.0;
+
+  print(empleados);
+  print(salario);
+}
