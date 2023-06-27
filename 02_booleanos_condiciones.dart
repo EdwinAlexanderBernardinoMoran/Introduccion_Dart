b40137c1020ad614f78bc7c49af7ae922afc741c
Edwin Alexander Bernardino Moran <edwinalexanderbernardinomoran@gmail.com>
2023-06-27 12:54:19 -0600

    Tipo de datos bolleanos y condiciones

---
+++ 02_booleanos_condiciones.dart
@@ -0,0 +1,12 @@
+void main() {
+  bool? isActive = null;
+
+  // El doble igual es un operador para hacer una comparacion entre dos objetos.
+  if (isActive != null) {
+    // print('Esta Activo');
+    print('Es Nulo');
+  } else {
+    // print('Esta inactivo');
+    print('No es Nulo');
+  }
+}
